9c41e729356f93d71b5394e0d1959015e7946d01
Julius Madsen <2330051117@protonmail.com>
2021-11-30 09:57:01 +0100

    random chaos

--- src/components/FoodPlan.js
+++ src/components/FoodPlan.js
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
 
+//We need to make a generate random button that fills out our *table?* with data/recipes.
+
+//We need to make a confirm button that saves the recipes to the database, and takes the username and weeknumber as 
+//parameters from the URL. And packs the recipes togheter as one big json string.
+
 function FoodPlan({facade, setErrorMessage}) {
 
     const[recipes, setRecipes] = useState([]);
 
     const updateRecipes = (data) => {
+        console.log(data)
         setRecipes(data.recipes);
     }
 
-    useEffect(() => {
-        facade.fetchData('recipe/weekly', updateRecipes, setErrorMessage);
-    }, [facade, setErrorMessage]);
+    const handleClick = (facade, setErrorMessage) => {
+       // facade.fetchData('recipe/weekly', updateRecipes, setErrorMessage);
+    };
 
     return ( 
         <div>
             <h1>Make your food plan </h1>
+            <ul>{recipes.map((r) => <li key={r.id}> {r.title}</li>)}</ul> 
+            <button onClick={handleClick(facade, setErrorMessage)}>Random</button>
         </div>
      );
 
