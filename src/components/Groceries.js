e1cf4c58af04dbe7856446e938b4da8c74e17319
Julius Madsen <2330051117@protonmail.com>
2021-12-08 22:21:03 +0100

    foodplans saved to a user is now loaded back in on user request, and user can now get a grocerylist

--- src/components/Groceries.js
+++ src/components/Groceries.js
@@ -1,33 +1,52 @@
-import { useState } from "react";
-import facade from "../facades/apiFacade";
+import { useState, useEffect } from "react";
 
-function Groceries() {
+function Groceries({ facade, setErrorMessage }) {
 
   const [ingredients, setIngredients] = useState([]);
   const[weeknumber, setWeekNumber] = useState(1);
 
-  useEffect(() => {
-    var currentdate = new Date();
-    var oneJan = new Date(currentdate.getFullYear(),0,1);
-    var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-    setWeekNumber(Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7));
-    facade.fetchData('grocerylist/' + facade.getUserName() + '/' + weeknumber, updateIngredients, setErrorMessage);
-  })
+  useEffect(()=>{
+        //This is just the calculations for the week number, used in our fetch and is being displayed in the table.
+        var currentdate = new Date();
+        var oneJan = new Date(currentdate.getFullYear(),0,1);
+        var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+        setWeekNumber(Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7));
+    
+        //This is the section for our fetch of the groceries.
+        //facade.fetchData('recipe/foodplan/' + facade.getUserName() + '/' + weeknumber, updateIngredients, setErrorMessage);
+  }, []);
+
+  const loadFoodPlan = (evt) => {
+    evt.preventDefault();
+    facade.fetchData('recipe/foodplan/' + facade.getUserName() + '/' + weeknumber, updateIngredients, setErrorMessage); 
+}
   
   const updateIngredients = (data) => {
     console.log(data)
-    setIngredients(data.ingredients);
-    setIsLoading(false)
+    console.log(data.extendedIngredients + "data.extendedIngredients")
+
+    setIngredients(data);
 }
+return (
+  <div>
+    {ingredients.map((recipe, index) => (
+      <div key={index}>
+        <h2>{recipe.title}</h2>
 
-  
-
-  
-    return (
-      <div>
-        <h1>Your Grocery List</h1>
+        {/* loop */}
+        <div>
+          {recipe.extendedIngredients.map((extendedIngredients, i) => (
+            <p key={i}>
+              {extendedIngredients.originalString}
+            </p>
+          ))}
+        </div>
       </div>
-    );
-  }
+    ))}
+    <button onClick={loadFoodPlan}>load Groceries</button>
+  </div>
+  
+);
+}
   
   export default Groceries;
