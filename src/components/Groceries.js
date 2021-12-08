1149ebf05bfb0fa3589239cb559f75e037393d66
AlexKenfelt <cph-ak428@cphbusiness.dk>
2021-12-08 11:29:48 +0100

    grocerylist

--- src/components/Groceries.js
+++ src/components/Groceries.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import facade from "../facades/apiFacade";
+
 function Groceries() {
+
+  const [ingredients, setIngredients] = useState([]);
+  const[weeknumber, setWeekNumber] = useState(1);
+
+  useEffect(() => {
+    var currentdate = new Date();
+    var oneJan = new Date(currentdate.getFullYear(),0,1);
+    var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+    setWeekNumber(Math.ceil(( currentdate.getDay() + 1 + numberOfDays) / 7));
+    facade.fetchData('grocerylist/' + facade.getUserName() + '/' + weeknumber, updateIngredients, setErrorMessage);
+  })
+  
+  const updateIngredients = (data) => {
+    console.log(data)
+    setIngredients(data.ingredients);
+    setIsLoading(false)
+}
+
+  
+
   
     return (
       <div>
-        <h1>Yo Yo Yo, Welcome to this awesome Grocery Page</h1>
+        <h1>Your Grocery List</h1>
       </div>
     );
   }
